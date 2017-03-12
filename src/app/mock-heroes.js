71fd787db6d13df42bccce2e1c40833f624c181a
Sarah Dudek <email": null>
2017-03-11 20:39:50 -0600

    Follow the tutorial IDK what's happening

---
+++ src/app/mock-heroes.js
@@ -0,0 +1,14 @@
+"use strict";
+exports.HEROES = [
+    { id: 11, name: 'Mr. Nice' },
+    { id: 12, name: 'Butts Gregson' },
+    { id: 13, name: 'Dopamine Restyline' },
+    { id: 14, name: 'Andrew' },
+    { id: 15, name: 'The Mysterious Mrs. Kitty' },
+    { id: 16, name: 'Dieter the Eater' },
+    { id: 17, name: 'Andrew S.' },
+    { id: 18, name: 'Old Man Sal' },
+    { id: 19, name: 'Harpoon Larry' },
+    { id: 20, name: 'M. L. R. Buegsly' }
+];
+//# sourceMappingURL=mock-heroes.js.map
